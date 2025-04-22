a0b487efbacfdc51c22a6ecfd28330c4fc4db918
Welets <welet991@gmail.com>
2025-04-22 19:15:20 +0300

    changed nginx-core back to nginx-full

--- puppet/zulip/manifests/localhost_camo.pp
+++ puppet/zulip/manifests/localhost_camo.pp
@@ -6,8 +6,8 @@ class zulip::localhost_camo {
   # Install nginx configuration to run camo locally
   file { '/etc/nginx/zulip-include/app.d/camo.conf':
     ensure  => file,
-    #require => Package['nginx-full'],
-    require => Package['nginx-core'],
+    require => Package['nginx-full'],
+    #require => Package['nginx-core'],
     owner   => 'root',
     group   => 'root',
     mode    => '0644',
